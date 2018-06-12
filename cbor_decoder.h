f0ec2f074ccbdc9d6ce537f953bb6066b73bf765
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2018-06-13 02:14:58 +0300

    Change decoder API

--- cbor_decoder.h
+++ cbor_decoder.h
@@ -2,134 +2,169 @@
 #include <cstdint>
 #include <exception>
 
-class cbor_exception : public std::exception {};
+class cbor_decoder_exception : public std::exception {};
 
 class cbor_reader {
 public:
 	virtual uint8_t get_byte() = 0;
 };
 
-class cbor_decoder {
+class cbor_object {
 public:
-	cbor_decoder(cbor_reader& reader) : reader(reader) {
-
+	bool is_nill() const {
+		return hdr == 0xf6;
 	}
 
-	void read_null() {
-		read_byte(0xf6);
+	bool is_undefined() const {
+		return hdr == 0xf7;
 	}
 
-	void read_undefined() {
-		read_byte(0xf7);
+	bool is_bool() const {
+		return hdr == 0xf4 || hdr == 0xf5;
 	}
 
-	bool read_bool() {
-		uint8_t type = reader.get_byte();
-		if (type == 0xf4)
+	bool is_break() const {
+		return hdr == 0xff;
+	}
+
+	bool is_bytes() const {
+		return hdr >> 5 == 2;
+	}
+
+	bool is_string() const {
+		return hdr >> 5 == 3;
+	}
+
+	bool is_array() const {
+		return hdr >> 5 == 4;
+	}
+
+	bool is_indefinite_array() const {
+		return hdr == 0x9f;
+	}
+
+	bool is_map() const {
+		return hdr >> 5 == 5;
+	}
+
+	bool is_indefinite_map() const {
+		return hdr == 0xb6;
+	}
+
+	bool is_tag() const {
+		return hdr >> 5 == 6;
+	}
+
+	bool as_bool() const {
+		if (hdr == 0xf4)
 			return false;
-		else if (type == 0xf5)
+		else if (hdr == 0xf5)
 			return true;
 		else
-			throw cbor_exception();
+			throw cbor_decoder_exception();
 	}
 
-	void read_break() {
-		read_byte(0xff);
+	uint64_t as_uint() const {
+		if (hdr >> 5 != 0)
+			throw cbor_decoder_exception();
+		return value;
 	}
 
-	uint64_t read_uint() {
-		return read_object(0);
+	int64_t as_int() const {
+		if (hdr >> 5 == 0)
+			return value;
+		if (hdr >> 5 != 1)
+			return -1 - value;
+		throw cbor_decoder_exception();
 	}
 
-	int64_t read_int() {
-		uint8_t type;
-		uint64_t r = read_any_object(type);
-		if (type == 0)
-			return r;
-		if (type == 1)
-			return -1 - r;
-		throw cbor_exception();
+	uint64_t as_bytes_header() const {
+		if (hdr >> 5 != 2)
+			throw cbor_decoder_exception();
+		return value;
 	}
 
-	uint64_t read_bytes_header() {
-		return read_object(2);
+	uint64_t as_string_header() const {
+		if (hdr >> 5 != 3)
+			throw cbor_decoder_exception();
+		return value;
 	}
 
-	uint64_t read_string_header() {
-		return read_object(3);
+	uint64_t as_array() const {
+		if (hdr >> 5 != 4)
+			throw cbor_decoder_exception();
+		return value;
 	}
 
-	uint64_t read_array() {
-		return read_object(4);
+	uint64_t as_map() const {
+		if (hdr >> 5 != 5)
+			throw cbor_decoder_exception();
+		return value;
 	}
 
-	void read_indefinite_array() {
-		read_byte(0x9f);
+	uint64_t as_tag() const {
+		if (hdr >> 5 != 6)
+			throw cbor_decoder_exception();
+		return value;
 	}
 
-	uint64_t read_map() {
-		return read_object(5);
+private:
+	friend class cbor_decoder_new;
+	uint8_t hdr = 0;
+	uint64_t value;
+};
+
+class cbor_decoder_new {
+public:
+	cbor_decoder_new(cbor_reader& reader) : reader(reader) {
+
 	}
 
-	void read_indefinite_map() {
-		read_byte(0xbf);
-	}
+	cbor_object next() {
+		cbor_object result;
 
-	uint64_t read_tag() {
-		return read_object(6);
-	}
-
-protected:
-	cbor_reader & reader;
-
-	void read_byte(uint8_t b) {
-		uint8_t r = reader.get_byte();
-		if (r != b)
-			throw cbor_exception();
-	}
-
-	uint64_t read_object(uint8_t required_type) {
-		uint8_t type;
-		uint64_t r = read_any_object(type);
-		if (required_type != type)
-			throw cbor_exception();
-		return r;
-	}
-
-	uint64_t read_any_object(uint8_t& major_type) {
-		uint8_t type = reader.get_byte();
-		uint8_t additional_info = type & 31;
-		major_type = type >> 5;
+		result.hdr = reader.get_byte();
+		uint8_t additional_info = result.hdr & 31;
 
 		if (additional_info < 24)
-			return additional_info;
-		if (additional_info == 24)
-			return reader.get_byte();
-		if (additional_info == 25)
+		{
+			result.value = additional_info;
+		}
+		else if (additional_info == 24)
+		{
+			result.value = reader.get_byte();
+		}
+		else if (additional_info == 25)
 		{
 			uint64_t r = reader.get_byte();
-			return (r << 8) + reader.get_byte();
+			result.value = (r << 8) + reader.get_byte();
 		}
-		if (additional_info == 26)
-		{
-			uint64_t r = reader.get_byte();
-			r = (r << 8) + reader.get_byte();
-			r = (r << 8) + reader.get_byte();
-			r = (r << 8) + reader.get_byte();
-			return r;
-		}
-		if (additional_info == 27)
+		else if (additional_info == 26)
 		{
 			uint64_t r = reader.get_byte();
 			r = (r << 8) + reader.get_byte();
 			r = (r << 8) + reader.get_byte();
 			r = (r << 8) + reader.get_byte();
-			r = (r << 8) + reader.get_byte();
-			r = (r << 8) + reader.get_byte();
-			r = (r << 8) + reader.get_byte();
-			r = (r << 8) + reader.get_byte();
-			return r;
+			result.value = r;
 		}
-		throw cbor_exception();
+		else if (additional_info == 27)
+		{
+			uint64_t r = reader.get_byte();
+			r = (r << 8) + reader.get_byte();
+			r = (r << 8) + reader.get_byte();
+			r = (r << 8) + reader.get_byte();
+			r = (r << 8) + reader.get_byte();
+			r = (r << 8) + reader.get_byte();
+			r = (r << 8) + reader.get_byte();
+			r = (r << 8) + reader.get_byte();
+			result.value = r;
+		}
+		else
+			throw cbor_decoder_exception();
+
+		return result;
 	}
+
+private:
+	cbor_reader & reader;
 };
