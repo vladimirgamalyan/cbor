bb4b886fcdc2af16a601c86104f9132a6d878d62
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2018-06-13 03:57:03 +0300

    Add std::stream adapters

---
+++ cbor_decoder_istream.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <istream>
+#include "cbor_decoder.h"
+
+class cbor_decoder_istream : public cbor_decoder
+{
+public:
+	cbor_decoder_istream(std::istream& s) : s(s) {}
+
+	std::string read_string_body(const cbor_object& o) {
+		uint64_t size = o.as_string_header();
+		std::string r((size_t)size, ' ');
+		s.read(&r[0], (std::streamsize)size);
+		return r;
+	}
+
+	std::string read_string() {
+		return read_string_body(next());
+	}
+
+	std::vector<uint8_t> read_bytes_body(const cbor_object& o) {
+		uint64_t size = o.as_bytes_header();
+		std::vector<uint8_t> r((size_t)size);
+		s.read((char*)&r[0], (std::streamsize)size);
+		return r;
+	}
+
+	std::vector<uint8_t> read_bytes() {
+		return read_bytes_body(next());
+	}
+
+private:
+	std::istream& s;
+	virtual uint8_t get_byte() override {
+		return (uint8_t)s.get();
+	}
+};
