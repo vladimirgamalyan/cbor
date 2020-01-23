f1913261115ad0920063a9d0fbaff84462184b5e
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2020-01-22 23:13:11 -0800

    Add tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <sstream>
 #include "../cbor_encoder_ostream.h"
 #include "../cbor_decoder_istream.h"
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
@@ -28,4 +29,27 @@ TEST_CASE("simple file read/write")
 		int64_t  i = decoder.read().as_int();
 		REQUIRE(i == 42);
 	}
+}
+
+size_t packed_integer_len(int64_t n)
+{
+	std::stringstream ss;
+	cbor_encoder_ostream encoder(ss);
+	encoder.write_int(n);
+	ss.seekg(0, ios::end);
+	return oss.tellg();
+}
+
+TEST_CASE("number length")
+{
+	REQUIRE(packed_integer_len(0) == 1);
+	REQUIRE(packed_integer_len(1) == 1);
+	REQUIRE(packed_integer_len(23) == 1);
+	REQUIRE(packed_integer_len(24) == 2);
+	REQUIRE(packed_integer_len(255) == 2);
+	REQUIRE(packed_integer_len(256) == 3);
+	REQUIRE(packed_integer_len(65535) == 3);
+	REQUIRE(packed_integer_len(65536) == 4);
+	REQUIRE(packed_integer_len(4294967295) == 4);
+	REQUIRE(packed_integer_len(4294967296) == 5);
 }
