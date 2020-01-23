9223f2cfc187bbaf5175d450391401fb0544eb6d
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2020-01-22 23:05:44 -0800

    Improve test with doctest test framework

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
 #include <fstream>
 #include "../cbor_encoder_ostream.h"
 #include "../cbor_decoder_istream.h"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 
-int main(int argc, char *argv[]) {
+TEST_CASE("simple file read/write")
+{
 	{
 		std::ofstream f("test.bin", std::fstream::binary);
 		f.exceptions(std::fstream::failbit | std::fstream::badbit);
@@ -13,15 +15,17 @@ int main(int argc, char *argv[]) {
 		encoder.write_string("Hello, World!");
 		encoder.write_int(42);
 	}
-
+	
 	{
 		std::ifstream f("test.bin", std::fstream::binary);
 		f.exceptions(std::fstream::failbit | std::fstream::badbit);
 		cbor_decoder_istream decoder(f);
 
-		std::cout << "array size: " << decoder.read().as_array() << std::endl;
-		std::cout << decoder.read_string()  << std::endl;
-		std::cout << decoder.read().as_int() << std::endl;
+		uint64_t l = decoder.read().as_array();
+		REQUIRE(l == 2);
+		std::string s = decoder.read_string();
+		REQUIRE(s == "Hello, World!");
+		int64_t  i = decoder.read().as_int();
+		REQUIRE(i == 42);
 	}
 }
-
